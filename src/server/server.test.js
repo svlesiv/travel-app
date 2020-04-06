8a9e739ff1fe19b4b54f9d86d2e70a214e7599fe
Svitlana Lesiv <lesivsvitlana@gmail.com>
2020-04-05 23:12:10 -0700

    test: add server test for post

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -6,8 +6,29 @@ const server = require("./server");
 describe("Get Endpoints", () => {
   // https://blog.campvanilla.com/jest-expressjs-and-the-eaddrinuse-error-bac39356c33a
   test("should return status 200 when getting `/`", async () => {
-    const res = await request(server).get('/');
+    const res = await request(server).get("/");
 
     expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe("Post Endpoints", () => {
+  const projectData = {
+    summary: "summary",
+    max: 10,
+    min: 5,
+    date: "04-05-2020",
+    daysDiff: 2,
+    country: "USA",
+    imgSrc: "https://address.png",
+    city: "Boston"
+  };
+
+  test("should post project data", async () => {
+    const res = await request(server)
+      .post("/add")
+      .send(projectData);
+
+    expect(res.body).toEqual(projectData);
   });
 });
